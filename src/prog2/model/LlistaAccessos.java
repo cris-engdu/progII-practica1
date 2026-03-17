f019a64ea346ced94d9fed3b6bc0bf3eb33d2099
jordi <jguerrpa119@alumnes.ub.edu>
2026-03-17 13:59:31 +0100

    practica2

---
+++ src/prog2/model/LlistaAccessos.java
@@ -0,0 +1,35 @@
+package prog2.model;
+
+import prog2.vista.ExcepcioCamping;
+
+public class LlistaAccessos implements InLlistaAccessos{
+    @Override
+    public void afegirAcces(Acces acc) throws ExcepcioCamping {
+
+    }
+
+    @Override
+    public void buidar() {
+
+    }
+
+    @Override
+    public String llistarAccessos(boolean estat) throws ExcepcioCamping {
+        return "";
+    }
+
+    @Override
+    public void actualitzaEstatAccessos() throws ExcepcioCamping {
+
+    }
+
+    @Override
+    public int calculaAccessosNoAccessibles() throws ExcepcioCamping {
+        return 0;
+    }
+
+    @Override
+    public float calculaMetresTerra() throws ExcepcioCamping {
+        return 0;
+    }
+}
---
+++ src/prog2/model/LlistaTasquesManteniment.java
@@ -0,0 +1,58 @@
+package prog2.model;
+
+public class LlistaTasquesManteniment implements InTascaManteniment{
+    @Override
+    public int getNum() {
+        return 0;
+    }
+
+    @Override
+    public TascaManteniment.TipusTascaManteniment getTipus() {
+        return null;
+    }
+
+    @Override
+    public Allotjament getAllotjament() {
+        return null;
+    }
+
+    @Override
+    public String getData() {
+        return "";
+    }
+
+    @Override
+    public int getDies() {
+        return 0;
+    }
+
+    @Override
+    public void setNum(int num_) {
+
+    }
+
+    @Override
+    public void setTipus(TascaManteniment.TipusTascaManteniment tipus_) {
+
+    }
+
+    @Override
+    public void setAllotjament(Allotjament allotjament_) {
+
+    }
+
+    @Override
+    public void setData(String data_) {
+
+    }
+
+    @Override
+    public void setDies(int dies_) {
+
+    }
+
+    @Override
+    public String getIluminacioAllotjament() {
+        return "";
+    }
+}
